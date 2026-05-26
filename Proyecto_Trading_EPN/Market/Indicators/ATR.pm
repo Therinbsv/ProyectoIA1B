9630edf77650d71dd65de9cfb92efd2074283d8a
Kam1k4ze54 <justin.ortiz@epnn.edu.ec>
2026-05-26 14:56:37 -0500

    Update ATR

--- Proyecto_Trading_EPN/Market/Indicators/ATR.pm
+++ Proyecto_Trading_EPN/Market/Indicators/ATR.pm
@@ -3,15 +3,22 @@ package Market::Indicators::ATR;
 use strict;
 use warnings;
 
+# Implementar el indicador técnico
+# Average True Range (ATR).
+
 sub new {
+
     my ($class, $period) = @_;
 
     my $self = {
 
+        # Período ATR
         period => $period,
 
+        # Serie completa ATR
         values => [],
 
+        # Último ATR calculado
         last_atr => undef,
     };
 
@@ -20,47 +27,67 @@ sub new {
     return $self;
 }
 
-# Actualiza ATR incrementalmente
+
+# update_last($market_data)
+# Calcula incrementalmente el último
+# valor ATR usando método Wilder.
+
 sub update_last {
+
     my ($self, $market_data) = @_;
 
+    # Total de velas disponibles
     my $size = $market_data->size();
 
+    # Se necesitan al menos 2 velas
     return if $size < 2;
 
+    # Vela actual
     my $current = $market_data->last_candle();
 
+    # Vela anterior
     my $previous =
         $market_data->get_candle($size - 2);
 
     return unless defined $current;
     return unless defined $previous;
 
-    my $hl =
-        $current->{high} - $current->{low};
+    # Cálculo True Range
 
+    # High - Low
+    my $hl =
+        $current->{high}
+        - $current->{low};
+
+    # High - Previous Close
     my $hc =
         abs(
             $current->{high}
             - $previous->{close}
         );
 
+    # Low - Previous Close
     my $lc =
         abs(
             $current->{low}
             - $previous->{close}
         );
 
+    # Selecciona máximo valor
     my $tr = $hl;
 
     $tr = $hc if $hc > $tr;
     $tr = $lc if $lc > $tr;
 
     # Wilder ATR incremental
+
+    # Primer ATR
     if (!defined $self->{last_atr}) {
 
         $self->{last_atr} = $tr;
     }
+
+    # ATR incremental
     else {
 
         $self->{last_atr} =
@@ -71,23 +98,35 @@ sub update_last {
             ) / $self->{period};
     }
 
+    # Guarda valor calculado
     push @{ $self->{values} },
         $self->{last_atr};
 }
 
+
+# get_values()
 # Devuelve todos los valores ATR
+# calculados.
+
 sub get_values {
+
     my ($self) = @_;
 
     return $self->{values};
 }
 
-# Reinicia indicador
+
+# reset()
+# Reinicia completamente el indicador.
+
 sub reset {
+
     my ($self) = @_;
 
+    # Limpia valores ATR
     $self->{values} = [];
 
+    # Reinicia ATR incremental
     $self->{last_atr} = undef;
 }
 
